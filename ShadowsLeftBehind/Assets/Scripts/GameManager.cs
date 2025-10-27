4fb11fce3aa85241dd869c1a5d509c3ede6b444e
emackjackson <ethanmackjackson@gmail.com>
2025-10-26 21:52:44 -0400

    Add methods to switch scenes

--- ShadowsLeftBehind/Assets/Scripts/GameManager.cs
+++ ShadowsLeftBehind/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
@@ -16,8 +17,53 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    //load the main menu first
+    private void Start()
+    {
+        StartCoroutine(LoadAndActivate("MainMenu"));
+    }
+
+    public void SwitchTo(string sceneName)
+    {
+        StartCoroutine(SwitchRoutine(sceneName));
+    }
+
+    private IEnumerator LoadAndActivate(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        while (!scene.isDone)
+            yield return null;
+
+        var loaded = SceneManager.GetSceneByName(sceneName);
+        SceneManager.SetActiveScene(loaded);
+    }
+
     public void SwitchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    private IEnumerator SwitchRoutine(string nextScene)
+    {
+        var load = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+
+        while (!load.isDone)
+            yield return null;
+
+        var newScene = SceneManager.GetSceneByName(nextScene);
+        SceneManager.SetActiveScene(newScene);
+
+        //unload previous scene contents
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var s = SceneManager.GetSceneAt(i);
+
+            if (s == null)
+                continue;
+
+            if (s.isLoaded && s.name != newScene.name && s.name != "Bootstrapper")
+                yield return SceneManager.UnloadSceneAsync(s);
+        }
     }
 }
