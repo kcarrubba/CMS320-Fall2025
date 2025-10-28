1a1e968fc452e88fb4087249fc0479b12d7979e7
emackjackson <ethanmackjackson@gmail.com>
2025-10-28 00:20:06 -0400

    Clamp player position to window with Collider half size

--- ShadowsLeftBehind/Assets/Scripts/PlayerController.cs
+++ ShadowsLeftBehind/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     Rigidbody2D rb;
-
+    Camera cam;
+    Vector2 halfSize;
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0f;
-        rb.freezeRotation = true;
+        this.rb = GetComponent<Rigidbody2D>();
+        this.rb.gravityScale = 0f;
+        this.rb.freezeRotation = true;
+        this.cam = Camera.main;
+
+        var collider = GetComponent<Collider2D>();
+
+        if (collider)
+            this.halfSize = collider.bounds.extents;
     }
 
     void FixedUpdate()
@@ -25,6 +32,14 @@ public class PlayerController : MonoBehaviour
         }
 
         dir = dir.normalized;
-        rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+        Vector3 pos = rb.position + dir * moveSpeed * Time.fixedDeltaTime;
+
+        Vector3 min = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+        Vector3 max = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
+
+        pos.x = Mathf.Clamp(pos.x, min.x + this.halfSize.x, max.x - this.halfSize.x);
+        pos.y = Mathf.Clamp(pos.y, min.y + this.halfSize.y, max.y - this.halfSize.y);
+
+        rb.MovePosition(pos);
     }
 }
